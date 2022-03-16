ff56e3dc5ae081e1511356aa8bf1b4c68a5fcccd
Carl Malagodi <malagodicarl@gmail.com>
2022-03-15 22:39:29 -0500

    added code for PlatformBalance auton action

--- src/v5_hal/firmware/include/auton/auton_actions/PlatformBalance.h
+++ src/v5_hal/firmware/include/auton/auton_actions/PlatformBalance.h
@@ -11,6 +11,10 @@
 class PlatformBalance : public AutonAction {
 private:
     Timer m_timer;
+    IDriveNode* m_drive_node;
+    InertialSensorNode* m_inertial_node;
+    ADIEncoderNode* m_encoder_node;
+    Eigen::Rotation2Dd m_roll;
 
 public:
     PlatformBalance(IDriveNode* drive_node, ADIEncoderNode* encoder_node, InertialSensorNode* inertial_node);
--- src/v5_hal/firmware/src/auton/auton_actions/PlatformBalance.cpp
+++ src/v5_hal/firmware/src/auton/auton_actions/PlatformBalance.cpp
@@ -1,15 +1,26 @@
 #include "auton/auton_actions/PlatformBalance.h"
 
-PlatformBalance::PlatformBalance(IDriveNode* drive_node, ADIEncoderNode* encoder_node, InertialSensorNode* inertial_node) {
-
+PlatformBalance::PlatformBalance(IDriveNode* drive_node, ADIEncoderNode* encoder_node, InertialSensorNode* inertial_node) :
+    m_drive_node(drive_node), 
+    m_encoder_node(encoder_node),
+    m_inertial_node(inertial_node)
+    {
 }
 
 void PlatformBalance::ActionInit() {
     m_timer.Start();
+    m_roll = m_inertial_node->getRoll();
+
 }
 
 AutonAction::actionStatus PlatformBalance::Action() {
-    
+    if (m_roll.angle() < 25) { //Phase 1: Driving onto the ramp
+        m_drive_node->setDriveVelocity(100, 0); //should act as a checker for when the robot is fully on the ramp
+    }
+    if(m_roll.angle() > 10) { //Phase 2: Driving until the ramp starts to tilt towards being balanced
+        m_drive_node->setDriveVelocity(100, 0); //should drive until the ramp starts to balance
+    }
+    m_drive_node->setDriveVelocity(0, 0); //Phase 3: Balancing at a standstill
 }
 
 void PlatformBalance::ActionEnd() {
