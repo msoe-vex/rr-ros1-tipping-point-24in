067c3d81c7be164a10753dd190af3e9ae5dcd5c4
Carl Malagodi <malagodicarl@gmail.com>
2022-03-15 21:05:47 -0500

    added platform includes

--- src/v5_hal/firmware/include/auton/auton_actions/PlatformBalance.h
+++ src/v5_hal/firmware/include/auton/auton_actions/PlatformBalance.h
@@ -2,13 +2,18 @@
 
 #include "lib-rr/auton/Auton.h"
 #include "lib-rr/util/Timer.h"
+#include "lib-rr/nodes/subsystems/IDriveNode.h"
+#include "lib-rr/nodes/sensor_nodes/InertialSensorNode.h"
+#include "lib-rr/auton/auton_actions/DriveStraightAction.h"
+#include "lib-rr/nodes/sensor_nodes/ADIEncoderNode.h"
+
 
 class PlatformBalance : public AutonAction {
 private:
     Timer m_timer;
 
 public:
-    PlatformBalance();
+    PlatformBalance(IDriveNode* drive_node, ADIEncoderNode* encoder_node, InertialSensorNode* inertial_node);
 
     void ActionInit();
 
