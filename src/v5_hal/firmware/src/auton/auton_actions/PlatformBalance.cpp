5ab149e4f7887a93c77a3046401709dc7c2d8a05
NathanDuPont <nathan.dupont01@gmail.com>
2022-03-22 19:45:50 -0500

    Updating submodules

--- src/v5_hal/firmware/src/auton/auton_actions/PlatformBalance.cpp
+++ src/v5_hal/firmware/src/auton/auton_actions/PlatformBalance.cpp
@@ -3,15 +3,12 @@
 PlatformBalance::PlatformBalance(IDriveNode* drive_node, ADIEncoderNode* encoder_node, InertialSensorNode* inertial_node) :
     m_drive_node(drive_node), 
     m_encoder_node(encoder_node),
-    m_inertial_node(inertial_node)
-
-    {
+    m_inertial_node(inertial_node) {
 }
 
 void PlatformBalance::ActionInit() {
     m_timer.Start();
     m_roll_offset = m_inertial_node->getRoll();
-
 }
 
 AutonAction::actionStatus PlatformBalance::Action() {
