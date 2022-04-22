a914be5d431fbed33d34f2063e1b5cc0474fbb7f
Dylan Powers <dylanpowers125@gmail.com>
2022-04-21 23:51:56 -0500

    fixing typo

--- src/v5_hal/firmware/src/auton/auton_routines/BasicAuton.cpp
+++ src/v5_hal/firmware/src/auton/auton_routines/BasicAuton.cpp
@@ -10,7 +10,7 @@ BasicAuton::BasicAuton(IDriveNode* drive_node, OdometryNode* odom_node, ADIEncod
 }
 
 void BasicAuton::AddNodes() {
-    AutonNode* forward_node = new AutonNode(10, new DriveStraightAction(m_drive_node, odom_node, 60, 50, 10));
+    AutonNode* forward_node = new AutonNode(10, new DriveStraightAction(m_drive_node, m_odom_node, 60, 50, 10));
     Auton::AddFirstNode(forward_node);
 
     //AutonNode* platformBalance_Node = new AutonNode(10, new PlatformBalance(m_drive_node, m_y_odom_encoder, m_inertial_node));
