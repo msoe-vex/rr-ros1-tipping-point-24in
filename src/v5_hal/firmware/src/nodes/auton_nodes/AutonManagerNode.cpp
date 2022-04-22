cf37f6364851f05e7ba65f3277bb2b7bed41a501
Dylan Powers <dylanpowers125@gmail.com>
2022-04-22 01:10:49 -0500

    fixed stuf

--- src/v5_hal/firmware/src/nodes/auton_nodes/AutonManagerNode.cpp
+++ src/v5_hal/firmware/src/nodes/auton_nodes/AutonManagerNode.cpp
@@ -1,11 +1,12 @@
 #include "nodes/auton_nodes/AutonManagerNode.h"
 
-AutonManagerNode::AutonManagerNode(NodeManager* node_manager, IDriveNode* drive_node, ADIEncoderNode* y_odom_encoder, InertialSensorNode* inertial_sensor) : 
+AutonManagerNode::AutonManagerNode(NodeManager* node_manager, OdometryNode* odom_node, IDriveNode* drive_node, ADIEncoderNode* y_odom_encoder, InertialSensorNode* inertial_sensor) : 
         Node(node_manager, 50),
+        m_odom_node(odom_node),
         m_drive_node(drive_node),
         m_y_odom_encoder(y_odom_encoder),
         m_inertial_sensor(inertial_sensor) {
-    m_basic_auton = new BasicAuton(m_drive_node, m_y_odom_encoder, m_inertial_sensor);
+    m_basic_auton = new BasicAuton(m_drive_node, m_odom_node, m_y_odom_encoder, m_inertial_sensor);
     selected_auton = m_basic_auton;
 }
 
