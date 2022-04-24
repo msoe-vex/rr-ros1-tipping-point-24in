59f3c451e45e43e1de9dbfa2f5d571109cc32f1e
Dylan Powers <dylanpowers125@gmail.com>
2022-04-23 19:30:46 -0500

    just gonna sneak this in here

--- src/v5_hal/firmware/src/nodes/BackClawNode.cpp
+++ src/v5_hal/firmware/src/nodes/BackClawNode.cpp
@@ -79,22 +79,29 @@ void BackClawNode::autonPeriodic() {
 void BackClawNode::periodic() {
     switch (m_state)
     {
-    case PIVOT_BACK:
-        m_pivot->setValue(1);
-        m_claw->setValue(1);
-    break;
+        case PIVOT_BACK:
+            m_claw->setValue(1);
 
-    case PIVOT_DOWN_CLAW_OPEN:
-        m_pivot->setValue(0);
-        m_claw->setValue(0);
-    break;
+            if (m_previousState == PIVOT_DOWN_CLAW_CLOSED) {
+                m_pivot->setValue(1);
+            } else if (m_stateChange && m_previousState == PIVOT_DOWN_CLAW_OPEN) {
+                m_timer.Start();
+            } else if (m_timer.Get() > 0.3) {
+                m_pivot->setValue(1);
+            }        
+            
+        break;
 
-    case PIVOT_DOWN_CLAW_CLOSED:
-        m_pivot->setValue(0);
-        m_claw->setValue(1);
-    break;
-    
-    default:
+        case PIVOT_DOWN_CLAW_OPEN:
+            m_pivot->setValue(0);
+
+            if (m_previousState == PIVOT_DOWN_CLAW_CLOSED) {
+                m_claw->setValue(0);    
+            } else if (m_stateChange && m_previousState == PIVOT_BACK) {
+                m_timer.Start();
+            } else if (m_timer.Get() > 0.7) {
+                m_claw->setValue(0);    
+            }       
         break;
     }
 }
