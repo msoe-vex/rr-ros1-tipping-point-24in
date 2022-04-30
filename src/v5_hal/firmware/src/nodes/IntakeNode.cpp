ba79aea719551786c97319349bf5bf2688532e9f
Dylan Powers <dylanpowers125@gmail.com>
2022-04-30 00:05:41 -0500

    fixes

--- src/v5_hal/firmware/src/nodes/IntakeNode.cpp
+++ src/v5_hal/firmware/src/nodes/IntakeNode.cpp
@@ -4,7 +4,7 @@ IntakeNode::IntakeNode(NodeManager* node_manager, std::string handle_name, Contr
         MotorNode* intake_motor, pros::controller_digital_e_t in_button, bool toggle) : IRollerIntakeNode(node_manager, handle_name), 
         m_controller(controller->getController()),
         m_intake_motor(intake_motor),
-        m_inButton(in_button),
+        m_in_button(in_button),
         m_toggle(toggle) {
     m_handle_name = handle_name.insert(0, "robot/");
     m_canReverse = false;
@@ -14,7 +14,7 @@ IntakeNode::IntakeNode(NodeManager* node_manager, std::string handle_name, Contr
         MotorNode* intake_motor, pros::controller_digital_e_t in_button, pros::controller_digital_e_t out_button) : IRollerIntakeNode(node_manager, handle_name), 
         m_controller(controller->getController()),
         m_intake_motor(intake_motor),
-        m_inButton(in_button),
+        m_in_button(in_button),
         m_out_button(out_button) {
     m_handle_name = handle_name.insert(0, "robot/");
     m_canReverse = true;
@@ -33,8 +33,8 @@ void IntakeNode::initialize() {
 }
 
 void IntakeNode::teleopPeriodic() {
-    if (toggle) {
-        bool upButtonCurrentState = m_controller->getController()->get_digital(m_in_button);
+    if (m_toggle) {
+        bool upButtonCurrentState = m_controller->get_digital(m_in_button);
 
         if (upButtonCurrentState == 1 && m_inButtonPreivousState == 0) {
             m_running = !m_running;
@@ -45,6 +45,8 @@ void IntakeNode::teleopPeriodic() {
         } else {
             setIntakeVoltage(0);
         }
+
+        m_inButtonPreivousState = upButtonCurrentState;
     } else {
         if (m_controller->get_digital(m_in_button)) { 
             setIntakeVoltage(MAX_MOTOR_VOLTAGE);
