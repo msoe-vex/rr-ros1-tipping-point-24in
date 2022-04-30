6ba17933dfa0a925a5ab1e67b93ffbbefc8b9f3e
Dylan Powers <dylanpowers125@gmail.com>
2022-04-29 22:50:39 -0500

    added feature flag to make one button intake toggleable

--- src/v5_hal/firmware/src/nodes/IntakeNode.cpp
+++ src/v5_hal/firmware/src/nodes/IntakeNode.cpp
@@ -1,10 +1,11 @@
 #include "nodes/IntakeNode.h"
 
 IntakeNode::IntakeNode(NodeManager* node_manager, std::string handle_name, ControllerNode* controller, 
-        MotorNode* intake_motor, pros::controller_digital_e_t in_button) : IRollerIntakeNode(node_manager, handle_name), 
+        MotorNode* intake_motor, pros::controller_digital_e_t in_button, bool toggle) : IRollerIntakeNode(node_manager, handle_name), 
         m_controller(controller->getController()),
         m_intake_motor(intake_motor),
-        m_in_button(in_button) {
+        m_inButton(in_button),
+        m_toggle(toggle) {
     m_handle_name = handle_name.insert(0, "robot/");
     m_canReverse = false;
 }
@@ -13,7 +14,7 @@ IntakeNode::IntakeNode(NodeManager* node_manager, std::string handle_name, Contr
         MotorNode* intake_motor, pros::controller_digital_e_t in_button, pros::controller_digital_e_t out_button) : IRollerIntakeNode(node_manager, handle_name), 
         m_controller(controller->getController()),
         m_intake_motor(intake_motor),
-        m_in_button(in_button),
+        m_inButton(in_button),
         m_out_button(out_button) {
     m_handle_name = handle_name.insert(0, "robot/");
     m_canReverse = true;
@@ -32,12 +33,26 @@ void IntakeNode::initialize() {
 }
 
 void IntakeNode::teleopPeriodic() {
-    if (m_controller->get_digital(m_in_button)) { 
-        setIntakeVoltage(MAX_MOTOR_VOLTAGE);
-    } else if (m_canReverse && m_controller->get_digital(m_out_button)) {
-        setIntakeVoltage(-1 * MAX_MOTOR_VOLTAGE);
+    if (toggle) {
+        bool upButtonCurrentState = m_controller->getController()->get_digital(m_in_button);
+
+        if (upButtonCurrentState == 1 && m_inButtonPreivousState == 0) {
+            m_running = !m_running;
+        }
+
+        if (m_running) {
+            setIntakeVoltage(MAX_MOTOR_VOLTAGE);
+        } else {
+            setIntakeVoltage(0);
+        }
     } else {
-        setIntakeVoltage(0);
+        if (m_controller->get_digital(m_in_button)) { 
+            setIntakeVoltage(MAX_MOTOR_VOLTAGE);
+        } else if (m_canReverse && m_controller->get_digital(m_out_button)) {
+            setIntakeVoltage(-1 * MAX_MOTOR_VOLTAGE);
+        } else {
+            setIntakeVoltage(0);
+        }
     }
 }
 
