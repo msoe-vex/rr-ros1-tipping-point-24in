dd269f6c08161f2c8b3d59137d29d2a4503b64e5
Dylan Powers <dylanpowers125@gmail.com>
2022-04-30 03:27:19 -0500

    added action to set high rung lift state

--- src/v5_hal/firmware/include/auton/auton_routines/BasicAuton.h
+++ src/v5_hal/firmware/include/auton/auton_routines/BasicAuton.h
@@ -6,16 +6,18 @@
 #include "lib-rr/nodes/sensor_nodes/InertialSensorNode.h"
 #include "lib-rr/auton/auton_actions/DriveStraightAction.h"
 #include "auton/auton_actions/PlatformBalance.h"
+#include "auton/auton_actions/SetHighRungLiftStateAction.h"
 #include "lib-rr/auton/auton_actions/UseClawAction.h"
 #include "lib-rr/auton/auton_actions/MoveLiftToPositionAction.h"
 #include "lib-rr/nodes/subsystems/ILiftNode.h"
 #include "lib-rr/nodes/subsystems/IClawNode.h"
 #include "lib-rr/util/Constants.h"
 #include "lib-rr/eigen/Eigen/Dense"
+#include "nodes/HighRungLiftNode.h"
 
 class BasicAuton : public Auton {
 public:
-    BasicAuton(IDriveNode* drive_node, OdometryNode* odom_node, ADIEncoderNode* y_odom_encoder, InertialSensorNode* inertial_sensor);
+    BasicAuton(IDriveNode* drive_node, OdometryNode* odom_node, HighRungLiftNode* high_rung_lift_node);
 
     void AddNodes();
 
@@ -24,5 +26,6 @@ private:
     ADIEncoderNode* m_y_odom_encoder;
     InertialSensorNode* m_inertial_sensor;
     OdometryNode* m_odom_node;
+    HighRungLiftNode* m_high_rung_lift_node;
 
 };
--- src/v5_hal/firmware/src/nodes/auton_nodes/AutonManagerNode.cpp
+++ src/v5_hal/firmware/src/nodes/auton_nodes/AutonManagerNode.cpp
@@ -1,11 +1,13 @@
 #include "nodes/auton_nodes/AutonManagerNode.h"
 
-AutonManagerNode::AutonManagerNode(NodeManager* node_manager, OdometryNode* odom_node, IDriveNode* drive_node, IClawNode* front_claw_node) : 
+AutonManagerNode::AutonManagerNode(NodeManager* node_manager, OdometryNode* odom_node, 
+        IDriveNode* drive_node, IClawNode* front_claw_node, HighRungLiftNode* high_rung_lift_node) : 
         Node(node_manager, 50),
         m_odomNode(odom_node),
         m_driveNode(drive_node),
-        m_frontClawNode(front_claw_node) {
-    m_match_auton = new MatchAuton(m_driveNode, m_odomNode, m_frontClawNode);
+        m_frontClawNode(front_claw_node),
+        m_high_rung_lift_node(high_rung_lift_node) {
+    m_match_auton = new BasicAuton(m_driveNode, m_odomNode, m_high_rung_lift_node);
     selected_auton = m_match_auton;
 }
 
