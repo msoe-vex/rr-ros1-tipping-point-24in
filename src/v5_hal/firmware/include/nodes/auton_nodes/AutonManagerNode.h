68c9221fe10706961e3e16f1f9e405bfb8a9671b
NathanDuPont <nathan.dupont01@gmail.com>
2022-05-01 12:06:05 -0500

    Updating LeftAuton for 24in to finish path and grab all rings

--- src/v5_hal/firmware/include/nodes/auton_nodes/AutonManagerNode.h
+++ src/v5_hal/firmware/include/nodes/auton_nodes/AutonManagerNode.h
@@ -5,7 +5,7 @@
 #include "lib-rr/auton/auton_routines/TestPoseAuton.h"
 #include "lib-rr/auton/auton_routines/TestTurnAuton.h"
 #include "auton/auton_routines/MatchAuton.h"
-#include "auton/auton_routines/MatchAuton2.h"
+#include "auton/auton_routines/LeftAuton.h"
 #include "auton/auton_routines/BasicAuton.h"
 #include "lib-rr/pathing/PathManager.h"
 #include "nodes/LiftNode.h"
@@ -17,7 +17,7 @@ private:
     OdometryNode* m_odomNode;
     IClawNode* m_frontClawNode;
     LiftNode* m_liftNode;
-    HighRungLiftNode* m_high_rung_lift_node;
+    HighRungLiftNode* m_highRungLiftNode;
     BackClawNode* m_backClawNode;
     IRollerIntakeNode* m_intakeNode;
     IRollerIntakeNode* m_conveyorNode;
@@ -26,7 +26,7 @@ private:
     Auton* m_test_path_auton;
     Auton* m_prog_skills;
     Auton* m_match_auton;
-    Auton* m_match_auton2;
+    Auton* m_leftAuton;
 
 public:
     AutonManagerNode(NodeManager* node_manager, OdometryNode* odom_node,
